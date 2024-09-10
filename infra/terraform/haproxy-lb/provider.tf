57d9c746fb0b6d188152e52951304b7c06693a03
Brian Corder <bjcorder@protonmail.com>
2024-09-10 10:42:58 -0500

    added tls-insecure flag

--- infra/terraform/haproxy-lb/provider.tf
+++ infra/terraform/haproxy-lb/provider.tf
@@ -11,4 +11,5 @@ provider "proxmox" {
     pm_api_url          = var.PROXMOX_API_URL
     pm_api_token_id     = var.PROXMOX_API_TOKEN_ID
     pm_api_token_secret = var.PROXMOX_API_TOKEN_SECRET
+    pm_tls_insecure = true
 }
